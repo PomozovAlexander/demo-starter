4702dc6b4400e172a8bf4231ac55a2ae3486098c
PomozovAlexander <pav1978@gmail.com>
2025-04-20 19:45:34 +0300

    add logger level from application.properties, change LoggerAutoConfiguration

--- starter_demo/src/main/java/com/aston/custom_spring_boot_example/starter_demo/config/LoggerAutoConfiguration.java
+++ starter_demo/src/main/java/com/aston/custom_spring_boot_example/starter_demo/config/LoggerAutoConfiguration.java
@@ -1,7 +1,10 @@
 package com.aston.custom_spring_boot_example.starter_demo.config;
 
+import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -11,14 +14,16 @@ import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
-@ConditionalOnClass
-@EnableConfigurationProperties
 public class LoggerAutoConfiguration {
+    @Value("${logging.custom.level:INFO}")
+    private String logLevel;
 
 
     @Bean
+    public Logger logger(){
+        Logger myLogger= (Logger) LoggerFactory.getLogger("MyCustomLogger");
+        myLogger.setLevel(Level.toLevel(logLevel));
+        return  myLogger;
 
-    public Logger myLogger(){
-        return (Logger) LoggerFactory.getLogger("MyCustomLogger");
     }
 }
